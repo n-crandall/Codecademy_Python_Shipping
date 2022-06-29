50c5fc589ecdfe7d8d7143c0e5f124f51cd03aba
n-crandall <99920990+n-crandall@users.noreply.github.com>
2022-06-29 13:34:06 -0600

    ground shipping conditional statement

--- shipping.py
+++ shipping.py
@@ -1,1 +1,13 @@
 weight = 3
+
+#ground shipping
+if weight <= 2:
+    cost_ground = weight * 4.5 + 20
+elif weight > 2 and weight <= 6:
+    cost_ground = weight * 9 + 20
+elif weight > 6 and weight <= 10:
+    cost_ground = weight * 12 + 20
+else:
+    cost_ground = weight * 14.25 + 20
+
+print(cost_ground)
