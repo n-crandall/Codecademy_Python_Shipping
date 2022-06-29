9eb1fc122129751ca534132b4e6a1bc9a7e52733
n-crandall <99920990+n-crandall@users.noreply.github.com>
2022-06-29 13:37:40 -0600

    changed cost values

--- shipping.py
+++ shipping.py
@@ -2,12 +2,12 @@ weight = 3
 
 #ground shipping
 if weight <= 2:
-    cost_ground = weight * 4.5 + 20
+    cost_ground = weight * 1.5 + 20
 elif weight > 2 and weight <= 6:
-    cost_ground = weight * 9 + 20
+    cost_ground = weight * 3 + 20
 elif weight > 6 and weight <= 10:
-    cost_ground = weight * 12 + 20
+    cost_ground = weight * 4 + 20
 else:
-    cost_ground = weight * 14.25 + 20
+    cost_ground = weight * 4.75 + 20
 
 print(cost_ground)
